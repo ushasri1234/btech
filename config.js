c897260071f267c55578b3fb4dd97a9c13c33ed9
ushasri1234 <160407692+ushasri1234@users.noreply.github.com>
2024-02-18 20:27:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['9177926140', 'btech bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
